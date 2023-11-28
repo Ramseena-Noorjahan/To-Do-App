3e532a16161b49270e844c17026e6213d0526f6d
Ramseenanoorjahan <ramseenanoorjahan@gmail.com>
2023-11-28 14:34:23 +0530

    upload a todo app.first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+
+import { useState } from 'react';
+import './App.css';
+import { set } from 'mongoose';
+
+function App() {
+
+  const [toDos,setToDos] = useState([])
+  const [toDo,setToDo] = useState('')
+    return (
+      <div className="app">
+        <div className="mainHeading">
+          <h1>ToDo List</h1>
+        </div>
+        <div className="subHeading">
+          <br />
+          <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+        </div>
+        <div className="input">
+          <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+          <i onClick={()=>setToDo([])}   className="fas fa-plus"></i>
+        </div>
+        <div className="todos">
+          <div className="todo">
+            <div className="left">
+              <input type="checkbox" name="" id="" />
+              <p>Rect tutorial</p>
+            </div>
+            <div className="right">
+              <i className="fas fa-times"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default App;
+
+
+
